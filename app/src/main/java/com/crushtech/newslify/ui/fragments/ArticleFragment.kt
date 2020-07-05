56193d8911340657d03e3dde7b2dbee3afbbc5bc
abGhost.cyber <xplendo@gmail.com>
2020-07-05 12:51:47 -0400

    61%

--- app/src/main/java/com/crushtech/newslify/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/crushtech/newslify/ui/fragments/ArticleFragment.kt
@@ -2,8 +2,10 @@ package com.crushtech.newslify.ui.fragments
 
 import android.content.Context
 import android.os.Bundle
-import android.view.View
+import android.view.*
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ImageView
 import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -12,6 +14,7 @@ import com.crushtech.newslify.ui.NewsActivity
 import com.crushtech.newslify.R
 import com.crushtech.newslify.ui.NewsViewModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
@@ -23,21 +26,26 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-class ArticleFragment : Fragment(R.layout.fragment_article) {
+class ArticleFragment : Fragment() {
     private lateinit var viewModel: NewsViewModel
     private val args: ArticleFragmentArgs by navArgs()
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
-    // private lateinit var bottomSheetBehavior:BottomSheetBehavior
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        (activity as NewsActivity).window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        val view = layoutInflater.inflate(R.layout.fragment_article, container, false)
         viewModel = (activity as NewsActivity).newsViewModel
         (activity as NewsActivity).supportActionBar?.hide()
-        //  (activity as NewsActivity).supportActionBar?.hide()
         val article = args.article
-        //get the argument from the generated arg class
 
-        fab_favorite.setOnClickListener {
+        view.findViewById<FloatingActionButton>(R.id.fab_favorite).setOnClickListener {
             viewModel.saveArticle(article)
             Snackbar.make(view, "Article saved successfully", Snackbar.LENGTH_SHORT).apply {
                 setAction("View") {
@@ -47,23 +55,32 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
             }
         }
 
-        fab_share.setOnClickListener {
+        view.findViewById<FloatingActionButton>(R.id.fab_share).setOnClickListener {
             Snackbar.make(view, "share button clicked", Snackbar.LENGTH_SHORT).show()
         }
 
         val bottomSheet: NestedScrollView = view.findViewById(R.id.my_bottom_sheet)
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-
+        val articleImg = view.findViewById<ImageView>(R.id.article_img)
         article.urlToImage?.let { image ->
             Picasso.get().load(image).fit().centerCrop()
-                .into(article_img)
+                .into(articleImg)
         }
-        scrollwebView.apply {
+        view.findViewById<WebView>(R.id.scrollwebView).apply {
 
             webViewClient = WebViewClient()
             article.url?.let { loadUrl(it) }
         }
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //  (activity as NewsActivity).supportActionBar?.hide()
+        //get the argument from the generated arg class
     }
 
     override fun onAttach(context: Context) {
@@ -79,6 +96,9 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
     override fun onStop() {
         (activity as NewsActivity).showBottomNavigation()
         (activity as NewsActivity).supportActionBar?.show()
+        (activity as NewsActivity).window.clearFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         super.onStop()
     }
 
